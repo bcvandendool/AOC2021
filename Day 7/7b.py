3cdbf5554251727de66ed72f919a45821cacb77d
Bart van den Dool <2692194+bcvandendool@users.noreply.github.com>
2021-12-07 20:37:15 +0100

    Day 7

---
+++ Day 7/7b.py
@@ -0,0 +1,25 @@
+def main():
+    locations = []
+
+    with open("7a_input.txt", "r") as f:
+        text = f.readline()
+        for num in text.split(","):
+            locations.append(int(num))
+
+    min_pos = -1
+    min_fuel = 999999999999999
+
+    for i in range(min(locations), max(locations) + 1):
+        sum = 0
+        for loc in locations:
+            sum = sum + ((pow(abs(loc - i), 2) + abs(loc - i)) / 2)
+        if sum < min_fuel:
+            min_pos = i
+            min_fuel = sum
+
+    print("min_pos: " + str(min_pos))
+    print("min_fuel: " + str(min_fuel))
+
+
+if __name__ == "__main__":
+    main()
